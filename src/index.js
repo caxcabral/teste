bf7b02d1780ec1e6546878ec1ad898ebac833664
Caio Xavier Cabral <caxcabral@pm.me>
2023-01-02 15:49:11 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Field(props) {
+  return (
+    <span className="field">{props.name}: props.value</span>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Input(props) {
+  return (
+    <div className="input-group">
+      <input className={props.className}>{props.text}</input>
+      <button className="input-button" onClick={props.onClick}></button>
+    </div>
+  )
+}
